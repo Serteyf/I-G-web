f1b836335955cf7a9382a82ef36224479c512fa8
Federico <Silva>
2022-04-13 20:50:42 -0300

    chore: eslint fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,18 +18,24 @@ const Home: NextPage = () => {
                 </h1>
 
                 <p className={styles.description}>
-                    Get started by editing <code className={styles.code}>pages/index.tsx</code>
+                    Get started by editing{" "}
+                    <code className={styles.code}>pages/index.tsx</code>
                 </p>
 
                 <div className={styles.grid}>
                     <a href="https://nextjs.org/docs" className={styles.card}>
                         <h2>Documentation &rarr;</h2>
-                        <p>Find in-depth information about Next.js features and API.</p>
+                        <p>
+                            Find in-depth information about Next.js features and API.
+                        </p>
                     </a>
 
                     <a href="https://nextjs.org/learn" className={styles.card}>
                         <h2>Learn &rarr;</h2>
-                        <p>Learn about Next.js in an interactive course with quizzes!</p>
+                        <p>
+                            Learn about Next.js in an interactive course with
+                            quizzes!
+                        </p>
                     </a>
 
                     <a
@@ -37,7 +43,9 @@ const Home: NextPage = () => {
                         className={styles.card}
                     >
                         <h2>Examples &rarr;</h2>
-                        <p>Discover and deploy boilerplate example Next.js projects.</p>
+                        <p>
+                            Discover and deploy boilerplate example Next.js projects.
+                        </p>
                     </a>
 
                     <a
@@ -45,7 +53,10 @@ const Home: NextPage = () => {
                         className={styles.card}
                     >
                         <h2>Deploy &rarr;</h2>
-                        <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
+                        <p>
+                            Instantly deploy your Next.js site to a public URL with
+                            Vercel.
+                        </p>
                     </a>
                 </div>
             </main>
@@ -58,7 +69,12 @@ const Home: NextPage = () => {
                 >
                     Powered by{" "}
                     <span className={styles.logo}>
-                        <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+                        <Image
+                            src="/vercel.svg"
+                            alt="Vercel Logo"
+                            width={72}
+                            height={16}
+                        />
                     </span>
                 </a>
             </footer>
